617f9bbab7f31e831969fed8d9cb302097bcdda8
sowhatt <34173130+sowhatt@users.noreply.github.com>
2026-08-18 16:23:42 +0200

    feat(auth): register JWT authentication

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { HealthController } from './health.controller';
 import { IamModule } from './iam/iam.module';
@@ -7,8 +8,9 @@ import { OrganizationsModule } from './organizations/organizations.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env', '../../.env'] }),
     DatabaseModule,
+    AuthModule,
     IamModule,
     OrganizationsModule,
   ],
