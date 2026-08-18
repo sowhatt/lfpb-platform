5b729a360653354499cd8bc8367c061e1448fe92
sowhatt <34173130+sowhatt@users.noreply.github.com>
2026-08-18 18:53:47 +0200

    feat(registry): provide registration workflow

--- apps/api/src/registries/registries.module.ts
+++ apps/api/src/registries/registries.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { IamModule } from '../iam/iam.module';
+import { RegistrationStatusService } from './registration-status.service';
 import { RegistriesController } from './registries.controller';
 import { RegistriesService } from './registries.service';
 
 @Module({
   imports: [AuthModule, IamModule],
   controllers: [RegistriesController],
-  providers: [RegistriesService],
+  providers: [RegistriesService, RegistrationStatusService],
 })
 export class RegistriesModule {}
