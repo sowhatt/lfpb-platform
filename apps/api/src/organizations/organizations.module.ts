1c4f2999bf51dccb4d91288f9ea1322b31211203
sowhatt <34173130+sowhatt@users.noreply.github.com>
2026-08-18 16:23:43 +0200

    feat(api): expose secured organizations endpoints

--- apps/api/src/organizations/organizations.module.ts
+++ apps/api/src/organizations/organizations.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { IamModule } from '../iam/iam.module';
+import { OrganizationsController } from './organizations.controller';
 import { OrganizationsService } from './organizations.service';
 
 @Module({
-  imports: [IamModule],
+  imports: [AuthModule, IamModule],
+  controllers: [OrganizationsController],
   providers: [OrganizationsService],
   exports: [OrganizationsService],
 })
