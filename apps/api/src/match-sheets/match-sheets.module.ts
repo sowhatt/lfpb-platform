94c2633689ab383a4b511df6de0d44dd28a4a388
sowhatt <34173130+sowhatt@users.noreply.github.com>
2026-09-07 15:44:46 +0200

    fix: import auth and iam in match sheets module

--- apps/api/src/match-sheets/match-sheets.module.ts
+++ apps/api/src/match-sheets/match-sheets.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { DatabaseModule } from '../database/database.module';
+import { IamModule } from '../iam/iam.module';
 import { MatchSheetsController } from './match-sheets.controller';
 import { MatchSheetsService } from './match-sheets.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [AuthModule, DatabaseModule, IamModule],
   controllers: [MatchSheetsController],
   providers: [MatchSheetsService],
   exports: [MatchSheetsService],
