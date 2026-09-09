110c831b1091bcee2e4647e109484b444b59e3de
sowhatt <34173130+sowhatt@users.noreply.github.com>
2026-09-09 11:04:59 +0200

    fix(api): provide auth dependencies to match events

--- apps/api/src/match-events/match-events.module.ts
+++ apps/api/src/match-events/match-events.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { DatabaseModule } from '../database/database.module';
 import { MatchEventsController } from './match-events.controller';
 import { MatchEventsService } from './match-events.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [AuthModule, DatabaseModule],
   controllers: [MatchEventsController],
   providers: [MatchEventsService],
 })
