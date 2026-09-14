c64f3bd4414fca4f368a763b336b5a538d82a34c
sowhatt <34173130+sowhatt@users.noreply.github.com>
2026-09-15 01:07:19 +0200

    feat: register match sheet signatures service

--- apps/api/src/match-sheets/match-sheets.module.ts
+++ apps/api/src/match-sheets/match-sheets.module.ts
@@ -3,13 +3,14 @@ import { AuthModule } from '../auth/auth.module';
 import { DatabaseModule } from '../database/database.module';
 import { IamModule } from '../iam/iam.module';
 import { MatchSheetPlayerControlsService } from './match-sheet-player-controls.service';
+import { MatchSheetSignaturesService } from './match-sheet-signatures.service';
 import { MatchSheetsController } from './match-sheets.controller';
 import { MatchSheetsService } from './match-sheets.service';
 
 @Module({
   imports: [AuthModule, DatabaseModule, IamModule],
   controllers: [MatchSheetsController],
-  providers: [MatchSheetsService, MatchSheetPlayerControlsService],
-  exports: [MatchSheetsService, MatchSheetPlayerControlsService],
+  providers: [MatchSheetsService, MatchSheetPlayerControlsService, MatchSheetSignaturesService],
+  exports: [MatchSheetsService, MatchSheetPlayerControlsService, MatchSheetSignaturesService],
 })
 export class MatchSheetsModule {}
