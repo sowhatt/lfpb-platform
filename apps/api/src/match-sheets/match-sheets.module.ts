7ec995297714ed98879c2bdf3aa3995ecce214dc
sowhatt <34173130+sowhatt@users.noreply.github.com>
2026-09-14 21:56:45 +0200

    feat: register player control service

--- apps/api/src/match-sheets/match-sheets.module.ts
+++ apps/api/src/match-sheets/match-sheets.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { DatabaseModule } from '../database/database.module';
 import { IamModule } from '../iam/iam.module';
+import { MatchSheetPlayerControlsService } from './match-sheet-player-controls.service';
 import { MatchSheetsController } from './match-sheets.controller';
 import { MatchSheetsService } from './match-sheets.service';
 
 @Module({
   imports: [AuthModule, DatabaseModule, IamModule],
   controllers: [MatchSheetsController],
-  providers: [MatchSheetsService],
-  exports: [MatchSheetsService],
+  providers: [MatchSheetsService, MatchSheetPlayerControlsService],
+  exports: [MatchSheetsService, MatchSheetPlayerControlsService],
 })
 export class MatchSheetsModule {}
